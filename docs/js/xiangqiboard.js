3ca6d58f53ee96cddd500f80a0120354aa9b94da
lengyanyu258 <lengyanyu258@outlook.com>
2018-08-22 22:38:48 +0800

    chore(docs/): upload website

--- docs/js/xiangqiboard.js
+++ docs/js/xiangqiboard.js
@@ -2,7 +2,7 @@
 // https://github.com/lengyanyu258/xiangqiboardjs/
 //
 // Copyright (c) 2017, Chris Oakman
-// Copyright (c) 2018, Shawn Sun
+// Copyright (c) 2018, @lengyanyu258
 // Released under the MIT license
 // https://github.com/lengyanyu258/xiangqiboardjs/blob/master/LICENSE.md
 
